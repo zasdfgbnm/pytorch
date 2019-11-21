b21ff9dbc0950e981754d8b74830fd6c2e8fded9
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-11-21 14:57:11 -0800

    fix

---
+++ benchmarks/tensor_iterator_benchmark/compare/new.py
@@ -0,0 +1,15 @@
+import torch
+
+
+def trivial_1d(shape, dtype=None, device=None):
+    return torch.empty(shape, dtype=dtype, device=device)
+
+def contiguous_last_dim(shape, dtype=None, device=None):
+    underlying_shape = tuple(s + 5 for s in shape)
+    t = trivial_1d(underlying_shape, dtype, device)
+    for i, s in enumerate(shape):
+        t = t.narrow(i, 0, s)
+    return t
+
+def non_contiguous(shape, dtype=None, device=None):
+    return contiguous_last_dim((*shape, 1), dtype, device).squeeze(-1)
