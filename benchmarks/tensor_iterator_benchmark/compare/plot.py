675e6abe60fbe9357838c399c807196e3acc1486
Xiang Gao <qasdfgtyuiop@gmail.com>
2019-11-25 14:14:00 -0800

    more

--- benchmarks/tensor_iterator_benchmark/compare/plot.py
+++ benchmarks/tensor_iterator_benchmark/compare/plot.py
@@ -1,2 +1,18 @@
+import pkg_resources
+
+def plot_data(data):
+    print(data)
+    pass
+
 def make_html(compare):
-    return 'html'
+    html_template_filename = pkg_resources.resource_filename(__name__, "template.html")
+    js_template_filename = pkg_resources.resource_filename(__name__, "template.js")
+    with open(html_template_filename) as f:
+        html_template = f.read()
+    with open(js_template_filename) as f:
+        js_template = f.read()
+    for title, experiment in compare.items():
+        for setup, data in experiment.items():
+            plot_data(data)
+    html = html_template.format(script=js_template)
+    return html
